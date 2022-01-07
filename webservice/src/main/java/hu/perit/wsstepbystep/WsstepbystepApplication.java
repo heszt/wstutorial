4bdd08ad6afb933afe7baf3588b42f59de402925
heszt <tamas.hesz38@gmail.com>
2022-01-07 17:27:22 +0100

    run in docker
    gradlew clean doI
    docker-compose up

--- webservice/src/main/java/hu/perit/wsstepbystep/WsstepbystepApplication.java
+++ webservice/src/main/java/hu/perit/wsstepbystep/WsstepbystepApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import hu.perit.spvitamin.spring.environment.EnvironmentPostProcessor;
+
 @SpringBootApplication
-@ComponentScan(basePackages = {"hu.perit.spvitamin", "hu.perit.wsstepbystep"})
+@ComponentScan(basePackages = { "hu.perit.spvitamin", "hu.perit.wsstepbystep" })
 public class WsstepbystepApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WsstepbystepApplication.class, args);
+
+		SpringApplication application = new SpringApplication(WsstepbystepApplication.class);
+		application.addListeners(new EnvironmentPostProcessor());
+		application.run(args);
 	}
 
 }
