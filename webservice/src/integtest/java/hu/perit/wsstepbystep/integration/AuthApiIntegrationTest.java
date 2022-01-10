4510a309f877e50338d299c3ef837acaa5447b38
heszt <tamas.hesz38@gmail.com>
2022-01-10 17:01:00 +0100

    3 integration test run properly

---
+++ webservice/src/integtest/java/hu/perit/wsstepbystep/integration/AuthApiIntegrationTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hu.perit.wsstepbystep.integration;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import hu.perit.spvitamin.core.StackTracer;
+import hu.perit.spvitamin.spring.auth.AuthorizationToken;
+import hu.perit.spvitamin.spring.config.LocalUserProperties;
+import hu.perit.spvitamin.spring.config.SpringContext;
+import hu.perit.spvitamin.spring.config.SysConfig;
+import hu.perit.spvitamin.spring.feignclients.SimpleFeignClientBuilder;
+import hu.perit.webservice.rest.client.WebserviceClient;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * @author Peter Nagy
+ */
+
+@ActiveProfiles({"default", "integtest"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Slf4j
+class AuthApiIntegrationTest
+{
+    @Test
+    void testRestEndpoint_withCorrectCredentials_whenUsing_DaoAuthenticationProvider()
+    {
+        log.debug("-----------------------------------------------------------------------------------------------------");
+        log.debug("testRestEndpoint_withCorrectCredentials_whenUsing_DaoAuthenticationProvider()");
+
+        try
+        {
+            LocalUserProperties localUserProperties = SpringContext.getBean(LocalUserProperties.class);
+
+            WebserviceClient webserviceClient = SimpleFeignClientBuilder.newInstance() //
+                    .requestInterceptor(new BasicAuthRequestInterceptor( //
+                            "admin",
+                            localUserProperties.getLocaluser().get("admin").getPassword())) //
+                    .build(WebserviceClient.class, SysConfig.getServerProperties().getServiceUrl());
+
+            // Calling the authentication endpoint
+            AuthorizationToken authentication = webserviceClient.authenticate(null);	//most nem kell processId
+            log.debug(authentication.getJwt());
+        }
+        catch (Exception e)			//nincs assert, nem kell ellenőrizni, mert ha rossz az auth, token, akkor exception lesz
+        {
+            log.error(StackTracer.toString(e));
+            Assertions.fail(e.toString());
+        }
+    }
+
+
+    @Test
+    void testRestEndpoint_withIncorrectUsername_whenUsing_DaoAuthenticationProvider()
+    {
+        log.debug("-----------------------------------------------------------------------------------------------------");
+        log.debug("testRestEndpoint_withIncorrectUsername_whenUsing_DaoAuthenticationProvider()");
+
+        WebserviceClient webserviceClient = SimpleFeignClientBuilder.newInstance().requestInterceptor(
+                new BasicAuthRequestInterceptor("valami_nem_letezo_username", "password")).build(WebserviceClient.class,
+                SysConfig.getServerProperties().getServiceUrl());
+
+        // Calling the authentication endpoint
+        Assertions.assertThrows(BadCredentialsException.class, () -> webserviceClient.authenticate(null));
+    }
+
+
+    @Test
+    void testRestEndpoint_withIncorrectPassword_whenUsing_DaoAuthenticationProvider()
+    {
+        log.debug("-----------------------------------------------------------------------------------------------------");
+        log.debug("testRestEndpoint_withIncorrectPassword_whenUsing_DaoAuthenticationProvider()");
+
+        WebserviceClient webserviceClient = SimpleFeignClientBuilder.newInstance().requestInterceptor(
+                new BasicAuthRequestInterceptor("admin", "wrong_password")).build(WebserviceClient.class,
+                SysConfig.getServerProperties().getServiceUrl());
+
+        // Calling the authentication endpoint
+        Assertions.assertThrows(BadCredentialsException.class, () -> webserviceClient.authenticate(null));
+    }
+
+}
